9867e2afc37616a6aba7f39b5b1d991d2201b99b
sofiisunreal <sophiekendi17@gmail.com>
2026-06-07 12:29:44 +0300

    while loop ass

--- lesson2e.py
+++ lesson2e.py
@@ -1,15 +1,20 @@
-# loop in a list 
+# loop in a list
 
 farmers=["Mary","John","Peter","Ian","Jane"]
 
 for farmer in farmers:
      print(farmer)
 
-milk=[12,51,15,47,54,45,45]
+milk = [10, 12, 8, 15, 9, 11, 13]
 total=0
-average=0
-     
- 
+for litres in milk:
+     total+=litres
+
+average= total /len(milk)
+print("The total milk collected is :",total)
+print ("The average milk collected is",average)
 
 
-     
+
+
+
--- lesson2f.py
+++ lesson2f.py
@@ -1,7 +1,7 @@
 # while loop-  executes a block of code a number of times based on a condition
 # initialize
 # while condition:
-#      block of code 
+#      block of code
 #      increment/decreament
 
 counter=1
@@ -9,10 +9,55 @@ while counter<=5:
      print(f"Hello {counter}")
      counter+=1
 
-pin=123
-password=int(input("Enter your pin: "))
-password=[password]
-trials=3
+mypin = "8989"
+trials = 3
+balance = 9000
 
+while trials > 0:
+    pin = input("Enter your Pin: ")
 
+    if pin == mypin:
+        print("Login successful ")
 
+        while True:
+            print("\n1. Check Balance")
+            print("2. Deposit")
+            print("3. Withdraw")
+            print("4. Exit")
+
+            choice = input("Enter your choice: ")
+
+            if choice == "1":
+                print("You have Ksh", balance, "in your account")
+
+            elif choice == "2":
+                add = float(input("Enter amount: "))
+                balance += add
+                print("You now have Ksh", balance)
+
+            elif choice == "3":
+                remove = float(input("Enter amount to withdraw: "))
+
+                if remove <= balance:
+                    balance -= remove
+                    print("You now have Ksh", balance)
+                else:
+                    print("Insufficient funds")
+
+            elif choice == "4":
+                print("Thank you for using our services")
+                break
+
+            else:
+                print("Invalid Choice")
+
+        break
+
+    else:
+        trials -= 1
+        print("Wrong Pin.")
+
+        if trials > 0:
+            print("You have", trials, "trials remaining")
+        else:
+            print("Account locked")
